a2007f1dc4fd5c543032abeae5030788c6a5bb89
wanghai <wanghai@shopex.cn>
2021-11-12 11:16:40 +0800

    test

--- beego/beego.go
+++ beego/beego.go
@@ -1,7 +1,9 @@
 package beego
 
 import "fmt"
+import "github.com/beego/bee"
 
-func Say() {
+func Run() {
 	fmt.Println("hello beego")
+	beego.Run()
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module gitee.com/haiiiiiii/gomod
 
 go 1.16
+
+require github.com/beego/bee v1.12.3
